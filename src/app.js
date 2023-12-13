cd579040d7c3fea108908a58436be23240072c78
robles1999 <robles1999@yahoo.com>
2023-12-13 16:41:12 -0500

    Added some api endpoints.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
 import { v4 as uuidv4 } from "uuid";
+import express from "express";
 
+const app = express();
+const PORT = 3000;
 
-console.log("Hello world!");
-console.log(uuidv4());
+const people = [
+    {
+        name: "Caleb",
+        industry: "music"
+    },
+    {
+        name: "John",
+        industry: "networking"
+    },
+    {
+        name: "Sal",
+        industry: "sports medicine"
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+});
+
+app.get('/api/customers', (req, res) => {
+    res.send({ customer: people });
+});
+
+app.post('/', (req, res) => {
+    res.send('This is a post request!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+});
